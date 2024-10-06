6990055c760c0668ad95c11bee5752383cdb5b07
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2024-10-06 23:15:54 +0330

    Add support for Razor files

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,54 @@
 import * as vscode from 'vscode';
 
+const fileMappings: { [key: string]: string } = {
+  '.xaml': '.xaml.cs',
+  '.razor': '.razor.cs',
+  '.xaml.cs': '.xaml',
+  '.razor.cs': '.razor'
+};
+
+function getMappedFilePath(filePath: string): string | null {
+  for (const [key, value] of Object.entries(fileMappings)) {
+    if (filePath.endsWith(key)) {
+      return filePath.replace(key, value);
+    }
+  }
+  return null;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
   let viewCodeDisposable = vscode.commands.registerCommand('extension.viewCode', async () => {
     const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
+    if (!editor) return;
 
     const document = editor.document;
-    const xamlPath = document.uri.fsPath;
-    const codeBehindPath = xamlPath.replace('.xaml', '.xaml.cs');
+    const filePath = document.uri.fsPath;
+    const codeBehindPath = getMappedFilePath(filePath);
+    if (!codeBehindPath) return;
 
     try {
       const codeDocument = await vscode.workspace.openTextDocument(codeBehindPath);
       await vscode.window.showTextDocument(codeDocument);
     } catch (error) {
-    // vscode.window.showErrorMessage(`Cannot find the code-behind file for ${xamlPath}`);
+      // vscode.window.showErrorMessage(`Cannot find the code-behind file for ${filePath}`);
     }
   });
 
   let viewMarkupDisposable = vscode.commands.registerCommand('extension.viewMarkup', async () => {
     const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
+    if (!editor) return;
 
     const document = editor.document;
-    const codeBehindPath = document.uri.fsPath;
-    const xamlPath = codeBehindPath.replace('.xaml.cs', '.xaml');
+    const filePath = document.uri.fsPath;
+    const markupPath = getMappedFilePath(filePath);
+    if (!markupPath) return;
 
     try {
-      const xamlDocument = await vscode.workspace.openTextDocument(xamlPath);
-      await vscode.window.showTextDocument(xamlDocument);
+      const markupDocument = await vscode.workspace.openTextDocument(markupPath);
+      await vscode.window.showTextDocument(markupDocument);
     } catch (error) {
-      // vscode.window.showErrorMessage(`Cannot find the XAML file for ${codeBehindPath}`);
+      // vscode.window.showErrorMessage(`Cannot find the markup file for ${filePath}`);
     }
   });
 
